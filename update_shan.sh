dbf3ee82e70d59dbead4b75e33f27239b92304e2
Shan Yu <shanyu1@g.ucla.edu>
2023-11-24 17:15:14 -0800

    add update_shan

---
+++ update_shan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Step 1: Take Relative .exe Path as Input
+exepath=$1 # assuming the first argument is the exe path
+
+# Step 2: Iterate Over Nodes and Copy the Executable
+nodes=(1 2 4 5 6 10 11)
+for i in "${nodes[@]}"; do
+    scp "$exepath" "zion-$i:/mnt/ssd/shan/grappa/build/Make+Release/${exepath}"
+done
+
+# Step 3: Report Finish and Update Done
+echo "Update completed for all nodes."
